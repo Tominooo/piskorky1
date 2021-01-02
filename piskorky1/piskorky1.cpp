9497d1626aed3d823ae291e4fda565f71e59f9c2
tomex <tomex@DESKTOP-E64NOGB>
2021-01-02 17:02:31 +0100

    pridavam void Input

--- piskorky1/piskorky1.cpp
+++ piskorky1/piskorky1.cpp
@@ -3,3 +3,21 @@
 
 #include <iostream>
 using namespace std;
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+int n;
+void Draw()
+{
+    system("cls");
+    cout << "Tic Tac Toe v1.0" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void Input()
+{
